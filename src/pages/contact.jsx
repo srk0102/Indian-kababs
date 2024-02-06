d03c44f02e9fc1b857fb2619709acbfc640b2cac
ramakrishnasiva128 <ramakrishnasiva128@gmail.com>
2024-02-06 21:56:00 +0000

    feat: added google earth package to the project

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export const Contact = () => {
   return (
-    <div>landing</div>
+    <>
+    <div className='flex justify-around contents-center h-screen'>
+      <div>
+      <h1 className='font-bold'>Contact-Us</h1>
+      <p>san romen</p>
+      <p>+1 9999999999</p>
+      </div>
+      <div className='contents-center'>
+        
+      </div>
+    </div>
+    </>
   )
 }
