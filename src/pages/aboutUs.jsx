8dfef9b03b0dc0f8afe612fbf46c8c002833f37d
Sreekar77 <sreekarreddy299@gmail.com>
2024-02-08 04:35:17 -0800

    added contact page

--- src/pages/aboutUs.jsx
+++ src/pages/aboutUs.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Footer, NavBar } from '../components'
+
 
 export const AboutUs = () => {
   return (
     <>
+    <NavBar/>
     
+    <Footer/>
+  
     </>
   )
 }
--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -4,11 +4,73 @@ export const Landing = () => {
 
   return (
     <>
-      <NavBar />
-      <div>
-        landing page
+    <NavBar />
+      <div className="px-52">
+        <div className=" ">
+        <div className="flex md:flex-row flex-col justify-around">
+          <div className="h-full p-20 justify-self-center md:self-center basis-1/2">
+            <h1 className="text-center text-xl sm:text-2xl bg-transparent">
+              INDIAN KEBABS AND WRAPS
+            </h1>
+          </div>
+          <div className="justify-items-center p-10 lg:p-28 basis-1/2">
+            <img
+              className="h-auto max-w-full rounded-lg"
+              src="https://drive.google.com/thumbnail?id=1w3ngbSpYB4Wk1NDrFCB6jVenNwGVtWEP&sz=w1000"
+              alt="img-1"
+            />
+          </div>
+          <hr />
+        </div>
+        <div className="flex md:flex-row-reverse flex-col justify-around ">
+          <div className="h-full p-20 justify-self-center md:self-center basis-1/2">
+            <h1 className="text-center text-xl sm:text-2xl bg-transparent">
+              INDIAN KEBABS AND WRAPS
+            </h1>
+          </div>
+          <div className="justify-items-center p-6 lg:p-28 basis-1/2">
+            <img
+              className="h-auto max-w-full rounded-lg"
+              src="https://drive.google.com/thumbnail?id=1w3ngbSpYB4Wk1NDrFCB6jVenNwGVtWEP&sz=w1000"
+              alt="img-1"
+            />
+          </div>
+        </div>
+        
+        
+        <div className="flex md:flex-row flex-col justify-around">
+          <div className="h-full p-20 justify-self-center md:self-center basis-1/2">
+            <h1 className="text-center text-xl sm:text-2xl bg-transparent">
+              INDIAN KEBABS AND WRAPS
+            </h1>
+          </div>
+          <div className="justify-items-center p-10 lg:p-28 basis-1/2">
+            <img
+              className="h-auto max-w-full rounded-lg"
+              src="https://drive.google.com/thumbnail?id=1w3ngbSpYB4Wk1NDrFCB6jVenNwGVtWEP&sz=w1000"
+              alt="img-1"
+            />
+          </div>
+          <hr />
+        </div>
+        <div className="flex md:flex-row-reverse flex-col justify-around ">
+          <div className="h-full p-20 justify-self-center md:self-center basis-1/2">
+            <h1 className="text-center text-xl sm:text-2xl bg-transparent">
+              INDIAN KEBABS AND WRAPS
+            </h1>
+          </div>
+          <div className="justify-items-center p-6 lg:p-28 basis-1/2">
+            <img
+              className="h-auto max-w-full rounded-lg"
+              src="https://drive.google.com/thumbnail?id=1w3ngbSpYB4Wk1NDrFCB6jVenNwGVtWEP&sz=w1000"
+              alt="img-1"
+            />
+          </div>
+        </div>
+        </div>
+        
       </div>
-      <Footer />
-    </>
-  )
-}
+    <Footer/>
+  </>
+  );
+};
